b44f08057758bdfeaa8c38913826a1a3696e913f
Luis Rendon <luisrendonsanz@gmail.com>
2026-08-14 10:48:25 -0700

    fix: agregue exepciones y mejora en las validaciones, nini proyecto terminado

--- weeks/week-01/java-fundamentals/src/StudentGradeManager/StudentGradeManager.java
+++ weeks/week-01/java-fundamentals/src/StudentGradeManager/StudentGradeManager.java
@@ -1,5 +1,6 @@
 package StudentGradeManager;
 
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -7,17 +8,19 @@ import java.util.stream.Collectors;
 
 public class StudentGradeManager {
     static Scanner in = new Scanner(System.in);
-    static private final double calificacionMax=100;
-    static private final double calificacionMin=70;
+    static private final double calificacionMax = 100;
+    static private final double calificacionMin = 70;
     static ArrayList<Double> calificaciones = new ArrayList<Double>();
 
     public static void main(String[] args) {
         menu();
+        in.close();
     }
 
     public static void menu() {
         int opc;
         do {
+
             System.out.println("Ingresa una opcion");
             System.out.println("1- Registrar calificacion\n" +
                     "2- Mostrar todas las calificaciones\n" +
@@ -27,18 +30,28 @@ public class StudentGradeManager {
                     "6- Indicar cuantos alumnos aprobaron\n" +
                     "7- Salir del programa "
             );
-
-            opc = in.nextInt();
-
+            try {
+                opc = in.nextInt();
+                in.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println("Ingresa algun numero de las opciones del menu");
+                in.nextLine();
+                opc = 0;
+            }
             switch (opc) {
                 case 1 -> registrarCalificacion();
                 case 2 -> mostrarCalificaciones();
                 case 3 -> calcularPromedio();
-                case 4 -> System.out.println(calificacionMax);
-                case 5 -> System.out.println(calificacionMin);
+                case 4 -> {
+                    System.out.println("Calificacion maxima: " + calificacionMax);
+                    continuar();
+                }
+                case 5 -> {
+                    System.out.println("Calificacion minima aprobatoria: " + calificacionMin);
+                    continuar();
+                }
                 case 6 -> alumnosAprobados();
                 case 7 -> {
-
                     System.out.println("Bye");
                     opc = 7;
                 }
@@ -48,39 +61,92 @@ public class StudentGradeManager {
     }
 
     static public void registrarCalificacion() {
-        System.out.println("Ingresa la calificacion que deseas agregar");
-        double registrarCalificacion = in.nextDouble();
-        calificaciones.add(registrarCalificacion);
+        try {
+            System.out.println("Ingresa la calificacion que deseas agregar");
+            double registrarCalificacion = in.nextDouble();
+            in.nextLine();
+            validarCalificacion(registrarCalificacion);
+            calificaciones.add(registrarCalificacion);
+            System.out.println("Calificacion registrada correctamente");
+        } catch (InputMismatchException e) {
+            System.out.print("Error: debes ingresar una calificacion numerica");
+            in.nextLine();
+        }
+        catch (IllegalArgumentException e){
+            System.out.println(e.getMessage());
+        }
+        continuar();
     }
 
     static public void mostrarCalificaciones() {
-        for (double cali : calificaciones) {
-            System.out.println(cali);
+        int i = 1;
+        if (calificaciones.isEmpty()) {
+            System.out.println("no hay calificaciones registradas");
+        } else {
+            for (double cali : calificaciones) {
+                System.out.println("calificacion " + i + ": " + cali);
+                i++;
+            }
         }
+        continuar();
     }
 
     static public void calcularPromedio() {
-        System.out.println("Ingresa cuantas materias son: ");
-        int materias = in.nextInt();
-        int i = 1;
-        double suma = 0;
-        while (i <= materias) {
-            System.out.println("Ingresa calificacion de la materia " + i);
-            suma += in.nextInt();
-            i++;
+        try {
+            System.out.println("Ingresa cuantas materias son: ");
+            int materias = in.nextInt();
+            if(materias<=0){
+                System.out.println("La cantidad de materias debe ser mayor a 0");
+                continuar();
+                return;
+            }
+            in.nextLine();
+            int i = 1;
+            double suma = 0;
+            double calificacion;
+            while (i <= materias) {
+                System.out.println("Ingresa calificacion de la materia " + i);
+                calificacion = in.nextDouble();
+                in.nextLine();
+                if (calificacion < 0 || calificacion > 100) {
+                    System.out.println("no puedes agregar una calificacion fuera de rango min:0 max:100");
+                } else {
+                    suma += calificacion;
+                }
+                i++;
+            }
+            double resultado = suma / materias;
+            System.out.println("Tu calificacion final es de: " + resultado);
+        } catch (InputMismatchException e) {
+            System.out.println("debes ingresar numero");
+            in.nextLine();
         }
-        double resultado = suma / materias;
-        System.out.println("Tu calificacion final es de: " + resultado);
+        continuar();
     }
 
     static public void alumnosAprobados() {
         int alumnoAprobado = 0;
-        for (int i = 0; i <= calificaciones.size(); i++) {
+        for (int i = 0; i < calificaciones.size(); i++) {
             if (calificaciones.get(i) >= calificacionMin) {
-                alumnoAprobado = i + 1;
+                alumnoAprobado++;
             }
         }
         System.out.println("total de alumnos aprobados " + alumnoAprobado);
+        continuar();
+    }
+
+    static public double validarCalificacion(double calificacion) {
+        if (calificacion > calificacionMax || calificacion < 0) {
+            throw new IllegalArgumentException(
+                    "La calificacion debe estar entre 0 y 100"
+            );
+        }
+        return calificacion;
+    }
+
+    static public void continuar() {
+        System.out.print("presiona enter para continuar");
+        in.nextLine();
     }
 
 }
